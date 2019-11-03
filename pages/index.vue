647fc22c92b2ebc0b8f6921c6303f1fc751cf497
Max Hunter <mphunter4@gmail.com>
2019-11-03 06:03:07 -0500

    initial implementation

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,94 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        baby-white-noise
-      </h1>
-      <h2 class="subtitle">
-        My funkadelic Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="container" :style="determineStyle">
+    <div v-for="noiseType in noiseTypes" @click="setNoiseType(noiseType)">
+      {{noiseType}}
     </div>
+    <button @click="pressButton()">
+      {{!this.playing ? 'play' : 'pause'}}
+    </button>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+  import Tone from 'tone'
 
-export default {
-  components: {
-    Logo
+  export default {
+    data () {
+      return {
+        playButtonText: 'Play',
+        playing: false,
+        selectedNoiseType: 'pink',
+        noiseTypes: ['pink', 'brown', 'white']
+      }
+    },
+    computed:{
+      determineStyle(){
+        let backgroundColor= ''
+        if (this.selectedNoiseType === 'pink') { backgroundColor = '#E9C1CD'}
+        if (this.selectedNoiseType === 'brown') { backgroundColor = '#443846'}
+        if (this.selectedNoiseType === 'white') {backgroundColor = 'white'}
+        return `background-color: ${backgroundColor}`
+      }
+    },
+    methods: {
+      pressButton () {
+        if (this.playing) {
+          this.stop()
+        } else {
+          this.start()
+        }
+      },
+      setNoiseType(noiseType){
+        this.selectedNoiseType = noiseType;
+        if(this.playing){
+          this.stop()
+        }
+
+      },
+      start () {
+        this.playing = true
+        this.noise = new Tone.Noise(this.selectedNoiseType)
+        this.noise.start().toMaster()
+      },
+      stop () {
+        this.noise.stop()
+        this.playing = false
+      }
+    }
   }
-}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    flex-direction: column;
+    transition: 1s ease-in-out;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .links {
+    padding-top: 15px;
+  }
 </style>
